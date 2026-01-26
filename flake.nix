ba09bb3ac1155bf50d3059405a0a3bdab1d82b7a
peterc-s <petercs@purelymail.com>
2026-01-26 21:29:04 +0000

    Formatting

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt.url = "github:numtide/treefmt-nix";
 
     naersk = {
       url = "github:nix-community/naersk";
@@ -20,6 +21,7 @@
     self,
     nixpkgs,
     flake-utils,
+    treefmt,
     naersk,
     fenix,
   }:
@@ -31,7 +33,14 @@
           cargo = toolchain;
           rustc = toolchain;
         };
+        treefmtConfig = treefmt.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.rustfmt.enable = true;
+          programs.alejandra.enable = true;
+        };
       in {
+        formatter = treefmtConfig.config.build.wrapper;
+
         packages.default = naersk'.buildPackage {
           src = ./.;
           buildInputs = with pkgs; [];
@@ -45,6 +54,7 @@
         };
 
         checks = {
+          formatting = treefmtConfig.config.build.check self;
           package = self.packages.${system}.default;
           clippy = naersk'.buildPackage {
             src = ./.;
