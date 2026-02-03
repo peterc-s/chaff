ae8833478c185ed6acd810a55944e1a6ef951186
peterc-s <petercs@purelymail.com>
2026-02-03 18:02:22 +0000

    Start on CLI

--- crates/chaff-cli/src/main.rs
+++ crates/chaff-cli/src/main.rs
@@ -1,6 +1,23 @@
-//! Dummy docs
+//! The command-line interface for interacting with the `chaff` anti website fingerprinting
+//! framework.
+
+#![expect(missing_docs)]
+
+use bpaf::Bpaf;
+
+/// Command-line interface options.
+#[derive(Debug, Clone, Bpaf)]
+#[bpaf(options, version)]
+pub enum CliOptions {
+    #[bpaf(command("capture"))]
+    /// Capture a traffic trace
+    Capture {
+        /// Name of the interface to use.
+        ifname: Option<String>,
+    },
+}
 
 /// Dummy docs
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", cli_options().run());
 }
