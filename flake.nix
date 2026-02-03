903b386ab50285d52a24e60fe30e717edf8a4c30
peterc-s <petercs@purelymail.com>
2026-02-03 17:22:00 +0000

    Use current latest toolchain for builds, but check with MSRV version in flake checks

--- flake.nix
+++ flake.nix
@@ -31,20 +31,35 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        toolchain = pkgs.rust-bin.stable."1.86.0".default;
+
+        # Toolchain for development use
+        toolchain = pkgs.rust-bin.stable.latest.default;
         naersk' = naersk.lib.${system}.override {
           cargo = toolchain;
           rustc = toolchain;
         };
+
+        # Toolchain for MSRV checking. MSRV is stated here and:
+        # - in the README
+        # - in the workspace Cargo.toml
+        # This should follow Arti.
+        toolchainMsrv = pkgs.rust-bin.stable."1.86.0".default;
+        naerskMsrv = naersk.lib.${system}.override {
+          cargo = toolchainMsrv;
+          rustc = toolchainMsrv;
+        };
+
         treefmtConfig = treefmt.lib.evalModule pkgs {
           projectRootFile = "flake.nix";
           programs.rustfmt.enable = true;
           programs.alejandra.enable = true;
         };
+
         buildInputs = with pkgs; [libpcap];
       in {
         formatter = treefmtConfig.config.build.wrapper;
 
+        # Default package for `nix build`
         packages.default = naersk'.buildPackage {
           inherit buildInputs;
           src = builtins.path {
@@ -54,17 +69,43 @@
           doDoc = true;
         };
 
-        devShells.default = pkgs.mkShell {
-          inherit buildInputs;
-          nativeBuildInputs = [
-            toolchain
-          ];
-          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+        devShells = {
+          # Default development shell
+          default = pkgs.mkShell {
+            inherit buildInputs;
+            nativeBuildInputs = [
+              toolchain
+            ];
+            RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+          };
+
+          # MSRV development shell
+          msrv = pkgs.mkShell {
+            inherit buildInputs;
+            nativeBuildInputs = [
+              toolchainMsrv
+            ];
+            RUST_SRC_PATH = "${toolchainMsrv}/lib/rustlib/src/rust/library";
+          };
         };
 
         checks = {
+          # This will just fail in CI/CD, but locally will actually
+          # do the formatting.
           formatting = treefmtConfig.config.build.check self;
+
+          # In CI/CD this will already be fulfilled as we build the package
+          # with `nix build` beforehand
           package = self.packages.${system}.default;
+
+          # Do a check with the toolchain set at the MSRV
+          msrv = naerskMsrv.buildPackage {
+            inherit buildInputs;
+            src = ./.;
+            mode = "check";
+          };
+
+          # Lint and test
           clippy = naersk'.buildPackage {
             inherit buildInputs;
             src = builtins.path {
