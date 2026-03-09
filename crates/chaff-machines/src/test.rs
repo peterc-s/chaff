1655bea18388762d69e65dfde215b6e686b8cb84
peterc-s <petercs@purelymail.com>
2026-03-09 18:19:19 +0000

    Transition probability ergonomics

--- crates/chaff-machines/src/test.rs
+++ crates/chaff-machines/src/test.rs
@@ -1,8 +1,16 @@
 //! Test machine used while developing the Chaff framework.
 
-use chaff::framework::{Machine, State};
+use chaff::framework::{Action, Event, Machine, State, Transition, TransitionProbs};
 
 /// Construct the test machine.
 pub fn construct_test_machine() -> Machine {
-    Machine::new(vec![State::new(None)])
+    let trans_probs = TransitionProbs::from_fn(|event| match event {
+        Event::SendNormal => Some(Transition(1, 0.5)),
+        Event::ReceiveNormal => None,
+    });
+
+    Machine::new(vec![
+        State::new(Some(trans_probs), Action::SendDecoy),
+        State::new(None, Action::SendDecoy),
+    ])
 }
--- crates/chaff/src/framework.rs
+++ crates/chaff/src/framework.rs
@@ -4,14 +4,24 @@
 #![expect(dead_code)]
 #![expect(missing_docs)]
 
+use std::ops::{Index, IndexMut};
+
 #[derive(Default)]
 pub struct Framework {
     machine: Machine,
+    runtime: MachineRuntime,
 }
 
 impl Framework {
     pub fn new(machine: Machine) -> Self {
-        Self { machine }
+        Self {
+            machine,
+            runtime: MachineRuntime::default(),
+        }
+    }
+
+    pub fn trigger_events(&self, _events: &[Event]) -> Box<[Action]> {
+        todo!()
     }
 }
 
@@ -27,14 +37,110 @@ impl Machine {
 }
 
 #[derive(Default)]
+pub struct MachineRuntime {
+    /// Index into the [`Machine::states`] array.
+    state: usize,
+}
+
+#[derive(Default)]
 pub struct State {
-    transitions: Option<Vec<Transition>>,
+    pub(crate) trans_probs: Option<TransitionProbs>,
+    pub(crate) action: Action,
 }
 
 impl State {
-    pub fn new(transitions: Option<Vec<Transition>>) -> Self {
-        Self { transitions }
+    pub fn new(trans_probs: Option<TransitionProbs>, action: Action) -> Self {
+        Self {
+            trans_probs,
+            action,
+        }
     }
 }
 
-pub struct Transition(pub usize, pub f32);
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Transition(
+    pub usize, // Index of state to transition to.
+    pub f32,   // Probability of transition.
+);
+
+pub struct TransitionProbs(pub [Option<Transition>; Event::COUNT]);
+
+impl TransitionProbs {
+    fn empty() -> Self {
+        Self([const { None }; Event::COUNT])
+    }
+
+    pub fn from_fn<F>(mut f: F) -> Self
+    where
+        F: FnMut(Event) -> Option<Transition>,
+    {
+        let mut probs = Self::empty();
+        for event in Event::ALL {
+            probs[event] = f(event);
+        }
+        probs
+    }
+}
+
+impl Index<Event> for TransitionProbs {
+    type Output = Option<Transition>;
+
+    fn index(&self, index: Event) -> &Self::Output {
+        &self.0[index.index()]
+    }
+}
+
+impl IndexMut<Event> for TransitionProbs {
+    fn index_mut(&mut self, index: Event) -> &mut Self::Output {
+        &mut self.0[index.index()]
+    }
+}
+
+#[derive(Default)]
+pub enum Action {
+    #[default]
+    SendDecoy,
+}
+
+// For easily working with events.
+macro_rules! enum_index {
+    ($name:ident { $($variant:ident),+ $(,)? }) => {
+        #[repr(usize)]
+        #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+        pub enum $name {
+            $($variant),+
+        }
+
+        impl $name {
+            pub const COUNT: usize = enum_index!(@count $($variant),+);
+
+            pub const ALL: [Self; Self::COUNT] = [
+                $(Self::$variant),+
+            ];
+
+            pub const fn index(self) -> usize {
+                self as usize
+            }
+        }
+    };
+
+    // token counting dark arts...
+    (@count $($tts:tt),*) => {
+        // this is using the array of units len method
+        // to count the "replace" of each token, which, from
+        // the name rule is the variants of the enum
+        // the replace rule simply replaces each variant with a unit
+        // therefore, if we have two variants, this should become
+        // <[()]>::len(&[(), ()]) which is 2, as it should be.
+        <[()]>::len(&[$(enum_index!(@replace $tts ())),*])
+    };
+
+    (@replace $_t:tt $sub:expr) => {$sub};
+}
+
+enum_index! {
+    Event {
+        SendNormal,
+        ReceiveNormal,
+    }
+}
